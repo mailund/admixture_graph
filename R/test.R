7c6b13aa86eadf163a6121a07310a50658c9b0ed
Thomas Mailund <mailund@birc.au.dk>
2015-04-22 17:21:02 +0200

    Calculating f statistics (symbolically at least)

--- R/test.R
+++ R/test.R
@@ -3,7 +3,7 @@
 agraph_children <- function(parents) {
   nodes <- rownames(parents)
   n <- length(nodes)
-  children <- matrix(0, n, n)
+  children <- matrix(FALSE, n, n)
   rownames(children) <- colnames(children) <- nodes
   
   for (child in nodes) {
@@ -17,11 +17,18 @@ agraph_children <- function(parents) {
   children
 }
 
-agraph <- function(parents) {
-  structure(list(parents = parents, children = agraph_children(parents)), 
-            class = "agraph")
+agraph <- function(parents, probs) {
+  graph <- structure(list(parents = parents, probs = probs,
+                     children = agraph_children(parents)), 
+                     class = "agraph")
+  graph
 }
 
+format_path <- function(graph, nodes) {
+  path <- data.frame(from = nodes[1:length(nodes)-1], to = nodes[2:length(nodes)])
+  path$prob <- mapply(function(f,t) graph$probs[f,t], path$from, path$to)
+  path
+}
 
 all_paths <- function(graph, src, dst) {
   src_idx <- which(src == rownames(graph$parents))
@@ -30,8 +37,8 @@ all_paths <- function(graph, src, dst) {
   X <- 1
   
   recurse_up <- function(node, path = c()) {
-    up_edges <- which(parents[node,] > 0)
-    down_edges <- which(children[node,] > 0)
+    up_edges <- which(graph$parents[node,] != 0)
+    down_edges <- which(graph$children[node,] != 0)
     
     sapply(up_edges, function(e) recurse_up(e, c(path, node)))
     sapply(down_edges, function(e) recurse_down(e, c(path, node)))
@@ -40,10 +47,10 @@ all_paths <- function(graph, src, dst) {
   recurse_down <- function(node, path) {
     if (node %in% path)  return(NULL)
     if (node == dst_idx) {
-      PATHS[[X]] <<- path
+      PATHS[[X]] <<- format_path(graph, c(path, node))
       X <<- X + 1
     } else {
-      down_edges <- which(children[node,] > 0)
+      down_edges <- which(graph$children[node,] != 0)
       sapply(down_edges, function(e) recurse_down(e, c(path, node)))
     }
   }
@@ -52,22 +59,82 @@ all_paths <- function(graph, src, dst) {
   PATHS
 }
 
+path_probability <- function(path) {
+  paste(path$prob, sep=" ", collapse="")
+}
+
+path_overlap <- function(path1, path2) {
+  probability = paste(path_probability(path1), path_probability(path2), sep='', collapse='')
+  rev_path2 <- data.frame(from = path2$to, to = path2$from, prob = path2$prob)
+  
+  list(prob = probability,
+       positive = merge(path1, path2)[,1:2],
+       negative = merge(path1, rev_path2)[,1:2])
+}
+
+path_overlap_weight <- function(overlap) {
+  if (nrow(overlap$positive) > 0) {
+    positive = paste(paste('[', overlap$positive$from, ':', overlap$positive$to, ']', sep=''),
+                     collapse=' + ')  
+  } else {
+    positive = ''
+  }
+  if (nrow(overlap$negative) > 0) {
+    negative = paste(paste('[', overlap$negative$from, ':', overlap$negative$to, ']', sep=''),
+                     collapse=' - ')  
+  } else {
+    negative = ''
+  }
+  
+  if (positive != '' && negative != '') {
+    paste(overlap$prob, ' * (', positive, ' - ', negative, ')', sep='')  
+  } else if (positive == '' && negative != '') {
+    paste('-', overlap$prob, ' * (', negative, ')', sep='')  
+  } else if (positive != '' && negative == '') {
+    paste(overlap$prob, ' * (', positive, ')', sep='')  
+  } else {
+    ''
+  }  
+}
+
+all_overlaps_weights <- function(paths1, paths2) {
+  overlap_weights <- c()
+  for (i in 1:length(paths1)) {
+    for (j in 1:length(paths2)) {
+      overlap <- path_overlap(paths1[[i]], paths2[[j]])
+      overlap_weights <- c(path_overlap_weight(overlap), overlap_weights)
+    }
+  }
+  paste(Filter(function(x) x != "", overlap_weights), sep=" + ")
+}
+
+f4 <- function(graph, W, X, Y, Z) {
+  WX <- all_paths(graph, W, X)
+  YZ <- all_paths(graph, Y, Z)
+  all_overlaps_weights(WX, YZ)
+}
+
+f3 <- function(graph, A, B, C) f4(graph, A, B, A, C)
+f2 <- function(graph, A, B)    f4(graph, A, B, A, B)
+
+parents <- matrix(FALSE, 8, 8)
+rownames(parents) <- colnames(parents) <- c("A", "B", "C", "AB", "BC", "ABC", "R", "O")
+parents["A","AB"] <- TRUE
+parents["C","BC"] <- TRUE
+parents["B","AB"] <- TRUE
+parents["B","BC"] <- TRUE
+parents["AB","ABC"] <- TRUE
+parents["BC","ABC"] <- TRUE
+parents["ABC","R"] <- TRUE
+parents["O","R"] <- TRUE
+
+weights <- matrix("", 8, 8)
+rownames(weights) <- colnames(weights) <- c("A", "B", "C", "AB", "BC", "ABC", "R", "O")
+weights["B","AB"] <- weights["AB","B"] <- 'a'
+weights["B","BC"] <- weights["BC","B"] <- '(1-a)'
 
 
-
-alpha <- 0.1
-parents <- matrix(0.0, 6,6)
-rownames(parents) <- colnames(parents) <- c("A", "B", "C", "AB", "BC", "R")
-parents["A","AB"] <- 1.0
-parents["C","BC"] <- 1.0
-parents["B","AB"] <- alpha
-parents["B","BC"] <- 1.0 - alpha
-parents["AB","R"] <- 1.0
-parents["BC","R"] <- 1.0
-
-G <- agraph(parents)
-
-
-AtoC <- all_paths(G, "A", "C")
-AtoB <- all_paths(G, "A", "B")
-
+graph <- agraph(parents, weights)
+f4(graph, "O", "A", "B", "C")
+f3(graph, "O", "A", "C")
+f3(graph, "O", "A", "B")
