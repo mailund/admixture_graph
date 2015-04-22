0683dca9b4a56893a8c7e86a34f52508efbc1c4b
Thomas Mailund <mailund@birc.au.dk>
2015-04-22 21:09:54 +0200

    Slightly better graph construction

--- R/agraph.R
+++ R/agraph.R
@@ -1,3 +1,38 @@
+
+
+#' Build a parent incidene matrix from an edge list
+#' 
+#' @param nodes The name of the nodes in the admxture graph
+#' @param parent_edges An n x 2 matrix where the first column is the child node and the second the parent.
+#'                          
+#' @return An incidence matrix for the parent structure of an admixture graph.
+agraph_parents <- function(nodes, parent_edges) {
+  n <- length(nodes)
+  parents <- matrix(FALSE, n, n)
+  rownames(parents) <- colnames(parents) <- nodes
+  for (row in 1:nrow(parent_edges)) {
+    parents[edges[row,1], edges[row,2]] = TRUE
+  }
+  parents
+}
+
+#' Build matrix of admixture proportions from an edge list
+#' 
+#' @param nodes The name of the nodes in the admxture graph
+#' @param admixture_weights An n x 3 matrix where the first column is the child node, the second the parent
+#'                          and the third the admixture weight on that edge.
+#'                          
+#' @return A matrix containing the admixture weights.
+agraph_weights <- function(nodes, admixture_weights) {
+  n <- length(nodes)
+  weights <- matrix("", n, n)
+  rownames(weights) <- colnames(weights) <- nodes
+  for (row in 1:nrow(admixture_weights)) {
+    weights[admixture_weights[row,1], admixture_weights[row,2]] = admixture_weights[row,3]
+  }
+  weights
+}
+
 #' Build the children incidence matrix from the parents incidence matrix.
 #' 
 #' @param parents The incidence matrix for the parent structure in an admixture graph.
@@ -22,9 +57,37 @@ agraph_children <- function(parents) {
 
 #' Create an admixture graph object.
 #' 
+#' @param nodes The name of the nodes in the admxture graph
+#' @param parent_edges An n x 2 matrix where the first column is the child node and the second the parent.
+#' @param admixture_weights An n x 3 matrix where the first column is the child node, the second the parent
+#'                          and the third the admixture weight on that edge.
+#'
+#' @return An admixture graph object.
+#' 
+#' @examples
+#' nodes <- c("A", "B", "C", "AB", "BC", "ABC", "R", "O")
+#' edges <- matrix(ncol = 2, byrow=TRUE,
+#'                 data = c("A", "AB",
+#'                          "C", "BC", 
+#'                          "B", "AB", 
+#'                          "B", "BC", 
+#'                          "AB", "ABC",
+#'                          "BC", "ABC",
+#'                          "ABC", "R",
+#'                          "O", "R"))
+#' admixture_proportions <- matrix(ncol = 3, byrow=TRUE,
+#'                                 data = c("B", "AB", "a", 
+#'                                          "B", "BC", "(1-a)"))
+#' 
+#' 
+#' graph <- agraph(nodes, edges, admixture_proportions)
+#' 
 #' @export
-agraph <- function(parents, probs) {
-  graph <- structure(list(parents = parents, probs = probs,
+agraph <- function(nodes, parent_edges, admixture_proportions) {
+  parents <- agraph_parents(nodes, parent_edges)
+  weigths <- agraph_weights(nodes, admixture_proportions)
+  
+  graph <- structure(list(parents = parents, probs = weights,
                           children = agraph_children(parents)), 
                      class = "agraph")
   graph
--- test.R
+++ test.R
@@ -1,22 +1,21 @@
 
-parents <- matrix(FALSE, 8, 8)
-rownames(parents) <- colnames(parents) <- c("A", "B", "C", "AB", "BC", "ABC", "R", "O")
-parents["A","AB"] <- TRUE
-parents["C","BC"] <- TRUE
-parents["B","AB"] <- TRUE
-parents["B","BC"] <- TRUE
-parents["AB","ABC"] <- TRUE
-parents["BC","ABC"] <- TRUE
-parents["ABC","R"] <- TRUE
-parents["O","R"] <- TRUE
-
-weights <- matrix("", 8, 8)
-rownames(weights) <- colnames(weights) <- c("A", "B", "C", "AB", "BC", "ABC", "R", "O")
-weights["B","AB"] <- weights["AB","B"] <- 'a'
-weights["B","BC"] <- weights["BC","B"] <- '(1-a)'
 
 
-graph <- agraph(parents, weights)
+nodes <- c("A", "B", "C", "AB", "BC", "ABC", "R", "O")
+edges <- matrix(ncol = 2, byrow=TRUE,
+                data = c("A", "AB",
+                         "C", "BC", 
+                         "B", "AB", 
+                         "B", "BC", 
+                         "AB", "ABC",
+                         "BC", "ABC",
+                         "ABC", "R",
+                         "O", "R"))
+admixture_proportions <- matrix(ncol = 3, byrow=TRUE,
+                                data = c("B", "AB", "a", "B", "BC", "(1-a)"))
+
+
+graph <- agraph(nodes, edges, admixture_proportions)
 f4(graph, "O", "A", "B", "C")
 f3(graph, "O", "A", "C")
 f3(graph, "O", "A", "B")
