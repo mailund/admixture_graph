f9b4a3d1fd798e3c1c26c77c4ef9bf8e04c30898
Thomas Mailund <mailund@birc.au.dk>
2015-04-24 16:20:10 +0200

    Separated overlap calculations from formatting

--- R/symbolic-f-statistics.R
+++ R/symbolic-f-statistics.R
@@ -1,8 +1,10 @@
-
-path_probability <- function(path) {
-  Filter(function(x) x != "", path$prob)
-}
-
+#' Collects the postive and negative overlap between two paths.
+#' 
+#' @param path1 The first path
+#' @param path2 The second path
+#' @return The (admixture) probability of seeing the two paths together with the positive
+#'         and negative edges in the overlap.
+#' @export
 path_overlap <- function(path1, path2) {
   rev_path2 <- data.frame(from = path2$to, to = path2$from, prob = path2$prob)
   list(prob = c(path_probability(path1), path_probability(path2)),
@@ -10,7 +12,31 @@ path_overlap <- function(path1, path2) {
        negative = merge(path1, rev_path2)[,1:2])
 }
 
-format_edge <- function(from, to) paste('[',from,':',to,']',sep='') # FIXME
+#' Get the list of overlaps of all paths.
+#' 
+#' @param paths1 Paths between one pair of leaves
+#' @param paths2 Paths between another pair of leaves
+#' @return A list of the overlaps of all combinations of paths from \code{paths1} and \code{paths2}.
+#' 
+#' @export
+all_path_overlaps <- function(paths1, paths2) {
+  n1 <- length(paths1)
+  n2 <- length(paths2)
+  overlaps <- list(rep(NA), n1*n2)
+  
+  idx <- 1
+  for (i in 1:n1) {
+    for (j in 1:n2) {
+      overlaps[[idx]] <- path_overlap(paths1[[i]], paths2[[j]])
+      idx <- idx + 1
+    }
+  }
+  overlaps
+}
+
+
+path_probability <- function(path) Filter(function(x) x != "", path$prob)
+format_edge <- function(from, to) paste('[',from,':',to,']',sep='')
 
 format_path_overlap <- function(overlap) {
   weight <- NULL
@@ -39,14 +65,8 @@ format_path_overlap <- function(overlap) {
 }
 
 format_all_overlaps <- function(paths1, paths2) {
-  overlap_weights <- c()
-  for (i in 1:length(paths1)) {
-    for (j in 1:length(paths2)) {
-      overlap <- path_overlap(paths1[[i]], paths2[[j]])
-      overlap_weights <- c(format_path_overlap(overlap), overlap_weights)
-    }
-  }
-  result <- paste(Filter(function(x) x != "0", overlap_weights), collapse = " + ")
+  overlaps <- vapply(all_path_overlaps(paths1, paths2), format_path_overlap, character(1))
+  result <- paste(Filter(function(x) x != "0", overlaps), collapse = " + ")
   if (length(result) > 0) result else '0'
 }
 
