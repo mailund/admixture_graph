495764447315dbcbadcb41a677e2f619bc70ad8f
Thomas Mailund <mailund@birc.au.dk>
2015-06-08 14:56:17 +0200

    Unlist the signs from get_graph_f4_sign

--- R/signs.R
+++ R/signs.R
@@ -100,8 +100,8 @@ get_graph_f4_sign <- function(graph, W, X, Y, Z) {
 #'   
 #' @export
 add_graph_f4_sign <- function(data, graph) {
-  signs <- Map(function(W,X,Y,Z) get_graph_f4_sign(graph, W, X, Y, Z),
-               data$W, data$X, data$Y, data$Z)
+  signs <- unlist(Map(function(W,X,Y,Z) get_graph_f4_sign(graph, W, X, Y, Z),
+                      data$W, data$X, data$Y, data$Z))
   data$graph_f4_sign <- signs
   data
 }
