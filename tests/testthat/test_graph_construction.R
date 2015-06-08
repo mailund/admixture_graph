7a482b209a7e1ba4ef01d33461b8f7fead0ddc14
Thomas Mailund <mailund@birc.au.dk>
2015-06-08 13:38:04 +0200

    Testing graph construction.
    Admixture proportions still missing...

--- tests/testthat/test_graph_construction.R
+++ tests/testthat/test_graph_construction.R
@@ -25,3 +25,32 @@ test_that("we can build a simple tree.", {
   expect_equal(names(which(graph$parents["AB",])), c("ABC"))
   expect_equal(names(which(graph$parents["ABC",])), character(0))
 })
+
+test_that("we can build a simple graph.", {
+  nodes <- c("A", "B", "C", "AC", "BC", "ABC")
+  edges <- matrix(ncol = 2, byrow=TRUE,
+                  data = c("A", "AC",
+                           "B", "BC",
+                           "C", "AC", "C", "BC",
+                           "AC", "ABC",
+                           "BC", "ABC"))
+  admixture_proportions <- NULL
+  
+  graph <- agraph(nodes, edges, admixture_proportions)
+  
+  expect_equal(graph$nodes, nodes)
+  
+  expect_equal(names(which(graph$children["A",])), character(0))
+  expect_equal(names(which(graph$children["B",])), character(0))
+  expect_equal(names(which(graph$children["C",])), character(0))
+  expect_equal(names(which(graph$children["AC",])), c("A", "C"))
+  expect_equal(names(which(graph$children["BC",])), c("B", "C"))
+  expect_equal(names(which(graph$children["ABC",])), c("AC", "BC"))
+  
+  expect_equal(names(which(graph$parents["A",])), c("AC"))
+  expect_equal(names(which(graph$parents["B",])), c("BC"))
+  expect_equal(names(which(graph$parents["C",])), c("AC", "BC"))
+  expect_equal(names(which(graph$parents["AC",])), c("ABC"))
+  expect_equal(names(which(graph$parents["BC",])), c("ABC"))
+  expect_equal(names(which(graph$parents["ABC",])), character(0))
+})
