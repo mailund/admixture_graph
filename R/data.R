b06033ff43b38851ddca97e9576eacf5facdf710
Thomas Mailund <mailund@birc.au.dk>
2015-06-09 20:52:42 +0200

    Documented bears data.

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Statistics for populations of bears
+#' 
+#' Computed $f_4(W,X;Y,Z)$ statistics for different populations of bears.
+#' 
+#' @format A data frame with 28 rows and 6 variables:
+#' \describe{
+#'  \item{W}{The W population}
+#'  \item{X}{The X population}
+#'  \item{Y}{The Y population}
+#'  \item{Z}{The Z population}
+#'  \item{D}{The $D$ ($f_4(W,X;Y,Z)$) statistics}
+#'  \item{Z.value}{The blocked jacknife Z values}
+#' }
+#' 
+#' @name bears
+#' @docType data
+#' @keywords data
+NULL
