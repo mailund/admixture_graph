9a88e58daddfb8a3c6dfa3f003aa86a6d8f71b28
Thomas Mailund <mailund@birc.au.dk>
2015-06-18 13:13:15 +0200

    Test of (basic) f statistics.

---
+++ tests/testthat/test_f_statistics.R
@@ -0,0 +1,24 @@
+context("f statistics")
+
+test_that("that the f statistics computes the correct paths overlaps.", {
+  leaves <- c("A", "B", "C", "D")
+  inner_nodes <- c("AC", "BC", "ABC")
+  edges <- parent_edges(c(edge("A", "AC"),
+                          edge("B", "BC"),
+                          admixture_edge("C", "AC", "BC"),
+                          edge("AC", "ABC"),
+                          edge("BC", "ABC"),
+                          edge("D", "ABC")))
+  admix <- admixture_proportions(c(admix_props("C", "AC", "BC", "a")))
+  graph <- agraph(leaves, inner_nodes, edges, admix)
+  
+  AB_paths <- all_paths(graph, "A", "B")
+  CB_paths <- all_paths(graph, "C", "B")
+  CD_paths <- all_paths(graph, "C", "D")
+    
+  expect_equal(all_path_overlaps(AB_paths, AB_paths), f2(graph, "A", "B"))
+  expect_equal(all_path_overlaps(CB_paths, CD_paths), f3(graph, "C", "B", "D"))
+  expect_equal(all_path_overlaps(AB_paths, CD_paths), f4(graph, "A", "B", "C", "D"))
+})
+  
+  
