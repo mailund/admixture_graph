34c161c1b15e0ee80a1ded1cfab08488bb571f38
Kalle Leppala <kalle.m.leppala@gmail.com>
2015-10-19 13:35:58 +0200

    Fixing a bug in build_edge_optimisation_matrix(), which only recognized stuff that depend on admix variables, not stuff that is always there.

--- R/fitting.R
+++ R/fitting.R
@@ -208,28 +208,29 @@ build_edge_optimisation_matrix <- function(data, graph, parameters
     statistic <- f4(graph, data$W[i], data$X[i], data$Y[i], data$Z[i])
     if (length(statistic) != 0) {
       for (j in seq(1, length(statistic))) {
-        if (length(statistic[[j]]$prob) != 0) {
-          admix_product <- ""
-          for (k in seq(1, length(statistic[[j]]$prob))) {
-            admix_product <- paste(admix_product, statistic[[j]]$prob[k], sep = "*")
+        if (length(statistic[[j]]$prob) == 0) {
+          statistic[[j]]$prob <- "1"
+        }
+        admix_product <- ""
+        for (k in seq(1, length(statistic[[j]]$prob))) {
+          admix_product <- paste(admix_product, statistic[[j]]$prob[k], sep = "*")
+        }
+        admix_product <- substring(admix_product, 2)
+        # Yeah I know this is a bit silly but the matrix is only created once.
+        if (NROW(statistic[[j]]$positive) > 0) { # Insert the positive stuff
+          for (k in seq(1, NROW(statistic[[j]]$positive))) {
+            edge_name <- fe(statistic[[j]]$positive[k, 1], statistic[[j]]$positive[k, 2])
+            edge_optimisation_matrix[i, edge_name] <- 
+              paste(edge_optimisation_matrix[i, edge_name],
+                    admix_product, sep = "+")
           }
-          admix_product <- substring(admix_product, 2)
-          # Yeah I know this is a bit silly but the matrix is only created once.
-          if (NROW(statistic[[j]]$positive) > 0) { # Insert the positive stuff
-            for (k in seq(1, NROW(statistic[[j]]$positive))) {
-              edge_name <- fe(statistic[[j]]$positive[k, 1], statistic[[j]]$positive[k, 2])
-              edge_optimisation_matrix[i, edge_name] <- 
-                paste(edge_optimisation_matrix[i, edge_name],
-                      admix_product, sep = "+")
-            }
-          }
-          if (NROW(statistic[[j]]$negative) > 0) { # Insert the negative stuff
-            for (k in seq(1, NROW(statistic[[j]]$negative))) {
-              edge_name <- fe(statistic[[j]]$negative[k, 1], statistic[[j]]$negative[k, 2])
-              edge_optimisation_matrix[i, edge_name] <- 
-                paste(edge_optimisation_matrix[i, edge_name], 
-                      admix_product, sep = "-")
-            }
+        }
+        if (NROW(statistic[[j]]$negative) > 0) { # Insert the negative stuff
+          for (k in seq(1, NROW(statistic[[j]]$negative))) {
+            edge_name <- fe(statistic[[j]]$negative[k, 1], statistic[[j]]$negative[k, 2])
+            edge_optimisation_matrix[i, edge_name] <- 
+              paste(edge_optimisation_matrix[i, edge_name], 
+                    admix_product, sep = "-")
           }
         }
       }
